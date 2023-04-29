2dea6f53c816a209769c249058995c0cb8c62cfd
developerMasum <developer.masum2001@gmail.com>
2023-04-29 09:56:44 +0600

    ready to diploy

--- index.js
+++ index.js
@@ -8,21 +8,50 @@
 //     console.log(`newspaper running on ${port}`);
 // })
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const port = 5000;
-const cors = require('cors')
-const categories = require('./data/categories.json')
+const cors = require("cors");
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('this is newspaper website... ')
+app.get("/", (req, res) => {
+  res.send("this is newspaper website... ");
+});
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  // console.log(id);
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+
+app.get('/categories/:id',(req,res)=>{
+  const id  = req.params.id ;
+  if (id == 0) {
+
+    res.send(news);
+  }
+  else{
+
+    const categoryNews = news.filter(n => (n.category_id) === id)
+    res.send(categoryNews)
+  }
+
+
 })
-app.get('/categories',(req,res)=>{
-    res.send(categories)
-})
+
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
